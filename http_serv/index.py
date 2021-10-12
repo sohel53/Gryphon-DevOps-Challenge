e34885d87e30ca850f3e2670bbb16966e922ccdb
sohel53 <maniyarmahebub0148@gmail.com>
2021-10-12 21:49:25 +0530

    secound commit

--- http_serv/index.py
+++ http_serv/index.py
@@ -1,9 +1,23 @@
 from flask import Flask
 import werkzeug
 werkzeug.cached_property = werkzeug.utils.cached_property
-from flask_restplus import Api, Resource
+from flask_restplus import Api, Resource, reqparse
+
 app = Flask(__name__)
 api = Api(app)
+
+parser = reqparse.RequestParser()
+parser.add_argument('name', help='Specify your name')
+
+
+@api.route('/<string:name>/')
+class name(Resource):
+    @api.doc(parser=parser)
+    def get(name):        
+        args = parser.parse_args()
+        name = args['name']
+        return "Hello " + name
+
 @api.route('/helloworld/')
 class HelloWorld(Resource):
     def get(self):
