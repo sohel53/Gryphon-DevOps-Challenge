3e8257305939d2da37dd75f53684115f614ee70e
sohel53 <maniyarmahebub0148@gmail.com>
2021-10-13 17:46:49 +0530

    git updtae

--- postman_testing.py
+++
@@ -1,8 +0,0 @@
-from flask import json
-
-from http_serv.index import api
-
-urlvars = False  # Build query strings in URLs
-swagger = True  # Export Swagger specifications
-data = api.as_postman(urlvars=urlvars, swagger=swagger)
-print(json.dumps(data))
