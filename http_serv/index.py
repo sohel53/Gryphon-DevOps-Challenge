408c3a74ccfc3f8bc83d8d5a1a441c62ac8875fc
sohel53 <maniyarmahebub0148@gmail.com>
2021-10-13 14:34:01 +0530

    fourth commit

--- http_serv/index.py
+++ http_serv/index.py
@@ -1,27 +1,38 @@
-from flask import Flask
+from flask import Flask , json
+import requests
+import subprocess
+import datetime
 import werkzeug
 werkzeug.cached_property = werkzeug.utils.cached_property
-from flask_restplus import Api, Resource, reqparse
+from flask_restplus import Api, Resource, reqparse, fields
 
 app = Flask(__name__)
-api = Api(app)
-
-parser = reqparse.RequestParser()
-parser.add_argument('name', help='Specify your name')
-
-
-@api.route('/<string:name>/')
-class name(Resource):
-    @api.doc(parser=parser)
-    def get(name):        
-        args = parser.parse_args()
-        name = args['name']
-        return "Hello " + name
+api = Api(app, version='1.0', title='Configure Http Server',
+    description='Completing DevOps challenge',
+)
 
 @api.route('/helloworld/')
 class HelloWorld(Resource):
     def get(self):
-        return "Hello Stranger"
-    
+        return "Hello stranger", 201
+
+parser = reqparse.RequestParser()
+parser.add_argument('name', help='Specify your name')
+
+@api.route('/hello/')
+class HelloWorld(Resource):
+    @api.doc(parser=parser)
+    def get(self):        
+        args = parser.parse_args()
+        name = args['name']
+        return "Hello " + name, 201
+
+@api.route('/versionz/')
+class HelloWorld(Resource):
+  def get(self):
+    full_hash = subprocess.check_output(['git', 'rev-parse', 'HEAD'])
+    full_hash = str(full_hash, "utf-8").strip()
+    return full_hash, 201
+
 if __name__ == '__main__':
-    app.run()
+    api.run(debug=True)
